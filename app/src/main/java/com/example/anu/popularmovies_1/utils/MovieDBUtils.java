8349b945900991e6662e6a57a79bdc17b1506c3f
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2017-12-08 12:40:07 +0530

    Add Movie DB constants and params

---
+++ app/src/main/java/com/example/anu/popularmovies_1/utils/MovieDBUtils.java
@@ -0,0 +1,24 @@
+package com.example.anu.popularmovies_1.utils;
+
+/**
+ * Created by Design on 08-12-2017.
+ */
+
+public class MovieDBUtils {
+
+    //api key to access TMDB apis
+    public static final String API_KEY = "";
+
+    //base url to access movies api
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+
+    public static final String KEY_MOVIE = "movie";
+    public static final String PARAM_VALUE_TOP_RATED = "top_rated";
+    public static final String PARAM_VALUE_POPULAR = "popular";
+
+    //api token parameter key
+    public static final String PARAM_API_KEY = "api_key";
+
+    public static final String URL_POSTER_PATH = "http://image.tmdb.org/t/p/w185/";
+    public static final String URL_BACKDROP_PATH = "http://image.tmdb.org/t/p/w780/";
+}
